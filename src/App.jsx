1ec1ebf6792ffaed5531dc5ef9e081b2fd0bf02c
Mihee Jeong-Burns <mihee.jeong@gmail.com>
2023-06-28 13:07:27 +1200

    set up react-router-dom and outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
