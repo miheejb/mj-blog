95efaa456fa008479a156bb67f226d5051e3f5e4
Mihee Jeong-Burns <mihee.jeong@gmail.com>
2023-07-08 11:07:51 +1200

    styling buttons with :first-child :last-child

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,17 +1,16 @@
-
 import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
 const Write = () => {
-const [value, setValue] = useState("");
+  const [value, setValue] = useState("");
 
   return (
     <div className="add">
       <div className="content">
         <input type="text" placeholder="title" />
         <div className="editorContainer">
-          <ReactQuill theme="snow" value={value} onChange={setValue} />;
+          <ReactQuill className="editor" theme="snow" value={value} onChange={setValue} />;
         </div>
       </div>
       <div className="menu">
@@ -24,7 +23,7 @@ const [value, setValue] = useState("");
             <b>Visibility:</b>Public
           </span>
           <input style={{ display: "none" }} type="file" name="" id="" />
-          <label htmlFor="file">Upload Image</label>
+          <label className="file" htmlFor="file">Upload Image</label>
           <div className="buttons">
             <button>Save as a draft</button>
             <button>Update</button>
