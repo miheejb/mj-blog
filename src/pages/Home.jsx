13b7c36467ce4dba07c786e1b61d00e728be8e7a
Mihee Jeong-Burns <mihee.jeong@gmail.com>
2023-07-07 20:02:27 +1200

    post.map

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom";
 
 const Home = () => {
 
@@ -33,7 +33,20 @@ const posts = [
   return (
     <div className="home">
     <div className="posts">
-        {/* {posts.map(post)=>} */}
+    {posts.map((post) => (
+        <div className="post" key={post.id}>
+            <div className="img">
+                <img src="{post.img}" alt="" />
+            </div>
+            <div className="content">
+                <Link to={`/post/${post.id}`}>
+                    <h1>{post.title}</h1>
+                </Link>
+            </div>
+        </div>
+
+    ))    
+    }
     </div>
     </div>
   )
